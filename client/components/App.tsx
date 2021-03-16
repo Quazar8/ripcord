da36446a4405cfd45906260ad1560223a096bba6
Quazar8 <barcode12d@gmail.com>
2021-03-16 23:59:42 +0200

    adding the App component to client side

---
+++ client/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            App section:
+            <h1>Hello from Reactssss</h1>
+        </div>
+    )
+}
+
+export default App
