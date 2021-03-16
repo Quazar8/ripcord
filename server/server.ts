84be3971f377effe340c62d8945caa9ce2dd3b1f
Quazar8 <barcode12d@gmail.com>
2021-03-16 23:23:32 +0200

    Adding express to the server side part of the project

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
-console.log('server code')
+import express from 'express'
+
+const app = express()
+const PORT = 8000
+
+app.get('/', (req, res) => {
+    res.send('hello frome xpress')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+})
