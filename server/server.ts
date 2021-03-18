83a038f08fdbd30557ed564c95d37ba2a3a7f9d6
Quazar8 <barcode12d@gmail.com>
2021-03-18 23:28:29 +0200

    Linking the user login route to the server

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
-import express, { Application } from 'express'
+import express from 'express'
 import path from 'path'
+
+import establishRouteEndpoints from './routes/routes.js'
 
 const app = express()
 const PORT = 8000
 
 app.use(express.static(path.join(path.dirname(''), '/build/client')))
+
+establishRouteEndpoints(app)
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
