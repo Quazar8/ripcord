1fc25cb04e762c26613d25af881cec970ad4d3ab
Quazar8 <barcode12d@gmail.com>
2021-03-19 23:59:37 +0200

    Intial react component for the navbar links

---
+++ client/components/navbar/NavLinks.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavLinks = () => {
+    return (
+        <div>
+            <a>Link 1</a>
+            <a>Link 2</a>
+        </div>
+    )
+}
+
+export default NavLinks
