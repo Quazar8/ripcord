96e3268a7feef936b87a0b075ca6271dc81f2624
Quazar8 <barcode12d@gmail.com>
2021-03-20 18:30:26 +0200

    Registering user on a valid server request

--- server/db/models.ts
+++ server/db/models.ts
@@ -4,11 +4,18 @@ const { Schema, model } = mongoose
 const UserSchema = new Schema ({
     username: { type: String, unique: true },
     password: String,
-    registeredAt: Date
+    registeredAt: { type: Date, default: () => Date.now() }
 })
+
+interface IUserModel {
+    username: string,
+    password: string,
+    registeredAt: Date
+}
 
 const User = model('User', UserSchema)
 
 export {
+    IUserModel,
     User
 }
--- server/routes/user/register.ts
+++ server/routes/user/register.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { User, IUserModel } from '../../db/models.js'
 import { errorResponse, successResponse } from '../../responses.js'
 
 const registerHandler = (req: Request, res: Response): void => {
@@ -7,8 +8,31 @@ const registerHandler = (req: Request, res: Response): void => {
         res.status(400).send(errorResponse('Missing username or password'))
         return
     }
-    
-    res.status(200).send(successResponse(null, 'Reached register endpoint'))
+
+    const saveUser = () => {
+        const userCandiate: IUserModel = {
+            username,
+            password,
+            registeredAt: new Date(Date.now())
+        }
+
+        const user = new User(userCandiate)
+        user.save().then(() => {
+            res.status(200).send(successResponse(null, 'User registered'))
+        }).catch(err => {
+            console.error(err)
+            res.status(500).send(errorResponse('Error ocured registering the user'))
+        })
+    }
+
+    User.findOne({ username }, (err, user) => {
+        if (user) {
+            res.status(400).send(errorResponse('User already exists'))
+            return
+        }
+
+        saveUser()
+    })
 }
 
 export default registerHandler
