1969d03269b98618ebc81555b79aaf0578827c35
Quazar8 <barcode12d@gmail.com>
2021-03-21 20:21:20 +0200

    Validating the login request

--- server/routes/user/login.ts
+++ server/routes/user/login.ts
@@ -2,6 +2,13 @@ import { Request, Response } from 'express'
 import { errorResponse, successResponse } from '../../responses.js'
 
 const loginHandler = (req: Request, res: Response): void => {
+    const { username, password } = req.body
+
+    if (!username || !password) {
+        res.status(400).send(errorResponse('Missing username or password'))
+        return
+    }
+
     res.status(200).send(successResponse(null, 'Reached login endpoint'))
 }
 
