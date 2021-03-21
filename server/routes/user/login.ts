05ccd33703b2a322f535e7b8acd3431f3e851418
Quazar8 <barcode12d@gmail.com>
2021-03-21 20:45:02 +0200

    Full validation of the user login request

--- server/routes/user/login.ts
+++ server/routes/user/login.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { User, IUserDoc } from '../../db/models.js'
 import { errorResponse, successResponse } from '../../responses.js'
 
 const loginHandler = (req: Request, res: Response): void => {
@@ -9,7 +10,14 @@ const loginHandler = (req: Request, res: Response): void => {
         return
     }
 
-    res.status(200).send(successResponse(null, 'Reached login endpoint'))
+    User.findOne({ username }).then((user: IUserDoc) => {
+        if (!user || user.password !== password) {
+            res.status(400).send(errorResponse('Incorrect user or password'))
+            return
+        }
+
+        res.status(200).send(successResponse({ user }, 'Reached login endpoint'))
+    })
 }
 
 export default loginHandler
