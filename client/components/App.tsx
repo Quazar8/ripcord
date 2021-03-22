9e65db360b09ac6202d960e5431ad91e3eeda87c
Quazar8 <barcode12d@gmail.com>
2021-03-22 18:50:10 +0200

    Adding react router dom to the project

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from './navbar/Navbar'
 
 const App = () => {
     return (
-        <div>
-            <Navbar />
-            <h1>Channel:</h1>
-            <div className = "received-msg"></div>
-            <label>
-                Send:
-                <input></input>
-            </label>
-        </div>
+        <BrowserRouter>
+            <div>
+                <Navbar />
+                <h1>Channel:</h1>
+                <div className = "received-msg"></div>
+                <label>
+                    Send:
+                    <input></input>
+                </label>
+            </div>
+        </BrowserRouter>
     )
 }
 
