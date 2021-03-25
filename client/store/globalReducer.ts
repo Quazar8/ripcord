28d72dbf407aef014a43831daeb986098bd1a35e
Quazar8 <barcode12d@gmail.com>
2021-03-25 23:56:14 +0200

    A hook reducer for the global state

---
+++ client/store/globalReducer.ts
@@ -0,0 +1,13 @@
+import { useReducer } from 'react'
+
+const initialState = {
+    notifications: []
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        default: return state
+    }
+}
+
+const [state, dispatch] = useReducer(reducer, initialState)
