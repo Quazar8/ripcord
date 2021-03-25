1199bf45aa9817533d7e7067fbcc1f1ad32e13c0
Quazar8 <barcode12d@gmail.com>
2021-03-25 23:46:43 +0200

    Adding an inital component for displaying error notifications

---
+++ client/components/others/notifications/ErrorNotification.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorNotification = () => {
+    return (
+        <div className = "notification error-notification">
+            Error Notification
+        </div>
+    )
+}
+
+export default ErrorNotification
