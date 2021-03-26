f67c9302e436fb5a4f442611ec8f4372f7bb668b
Quazar8 <barcode12d@gmail.com>
2021-03-26 19:59:58 +0200

    Some global action types and  add notification action generator

--- client/store/globalActions.ts
+++ client/store/globalActions.ts
@@ -1,4 +1,22 @@
-export type ActionType {
-    type: string
-    payload: any
+export type Action<T, Enumerator> = {
+    type: Enumerator
+    payload: T
+}
+
+export enum GlobalActionTypes {
+    PushNotification,
+    RemoveNotification
+}
+
+export type Notification = {
+    msg: string
+    type: 'info' | 'error' | 'success'
+}
+
+export const addNotification =
+        (data: Notification): Action<Notification, GlobalActionTypes> => {
+    return {
+        type: GlobalActionTypes.PushNotification,
+        payload: data
+    }
 }
