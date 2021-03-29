c7d29e567c30766a5957f4b5886f4fccc96220dd
Quazar8 <barcode12d@gmail.com>
2021-03-29 18:50:03 +0300

    Connecting the notifications container with the store

--- client/components/others/notifications/NotificationsContainer.tsx
+++ client/components/others/notifications/NotificationsContainer.tsx
@@ -1,14 +1,47 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { addNotification, Notification } from '../../../store/globalActions'
+import { connect, MapStateFn, MapDispatchFn } from '../../../store/connect'
 
-const NotificationsContainer = () => {
-    
+type Props = {
+    notifications: Notification[],
+    pushNotification: (data: Notification) => void
+}
+
+const NotificationsContainerView = ({ notifications, pushNotification }: Props) => {
+    const appendNotification = () => {
+        pushNotification({
+            id: '' + Date.now(),
+            msg: 'test notification',
+            type: 'info'
+        })
+    }
 
     return (
         <section className = "notification-container">
-            <button onClick = { () => {}}>Add Notification</button>
+            {
+                notifications.map((n, i) => (
+                    <h3 key = { i }>{ n.msg + ' ' + i }</h3>
+                ))
+            }
+            <button onClick = { appendNotification }>Add Notification</button>
         </section>
     )
 }
+
+const mapState: MapStateFn = state => {
+    return {
+        notifications: state.global.notifications
+    }
+}
+
+const mapDispath: MapDispatchFn = dispatch => {
+    return {
+        pushNotification: (data: Notification) => {
+            dispatch(addNotification(data))
+        }
+    }
+}
+
+const NotificationsContainer = connect(mapState, mapDispath)(NotificationsContainerView)
 
 export default NotificationsContainer
