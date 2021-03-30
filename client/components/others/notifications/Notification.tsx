ca7fdc50da34edf4973e040ffd12349ac1f10e46
Quazar8 <barcode12d@gmail.com>
2021-03-30 23:52:58 +0300

    Defining the notification props type

--- client/components/others/notifications/Notification.tsx
+++ client/components/others/notifications/Notification.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-const Notification = ({ msg, type }) => {
+type Props = {
+    msg: string,
+    type: 'info' | 'success' | 'error'
+}
+
+const Notification = ({ msg, type }: Props) => {
     return (
         <div className = "notification">
             Success Notification
