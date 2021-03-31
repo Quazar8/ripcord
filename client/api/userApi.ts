ed58b34c53fb70fd2794d98b6eb4f0e8eb47fb0e
Quazar8 <barcode12d@gmail.com>
2021-03-31 23:35:40 +0300

    Using the correct fetch link in the client side user api

--- client/api/userApi.ts
+++ client/api/userApi.ts
@@ -2,6 +2,6 @@ import { baseUrl, postQueryOptions } from './reqOptions'
 import { LoginEntryObj } from '../../server/routes/user/userTypes'
 
 export const loginServer = async (data: LoginEntryObj) => {
-    return await fetch(baseUrl + '/login', postQueryOptions(data))
+    return await fetch(baseUrl + '/user/login', postQueryOptions(data))
         .then(res => res.json())
 }
