8e63fddf2c1ecc1bcdabfd2929b9a4b8d9d6c556
Quazar8 <barcode12d@gmail.com>
2021-03-31 23:31:04 +0300

    Express to respond with the defautl html as a fallback

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,9 +1,13 @@
-import { Application } from 'express'
-
+import { Application, Request, Response } from 'express'
+import path from 'path'
 import enableUserRoutes from './user/UserRoutes.js'
 
 const establishRouteEndpoints = (app: Application): void => {
     enableUserRoutes(app)
+
+    app.get('*', (req: Request, res: Response) => {
+        res.sendFile(path.resolve('./build/client/index.html'))
+    })
 }
 
 export default establishRouteEndpoints
