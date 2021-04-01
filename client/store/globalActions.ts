554f154a3e67c0599931ff3e10c9c95c712cbbba
Quazar8 <barcode12d@gmail.com>
2021-04-01 20:40:09 +0300

    Creating Notification type object in the action creator

--- client/store/globalActions.ts
+++ client/store/globalActions.ts
@@ -15,10 +15,19 @@ export type GlobalPayloads = Notification
 
 export type GlobalAction = Action<GlobalActionTypes, GlobalPayloads>
 
+type AddNotificationAction = Action<GlobalActionTypes.PushNotification, Notification>
+
 export const addNotification =
-        (data: Notification): GlobalAction => {
+        (type: Notification['type'], msg: Notification['msg']): AddNotificationAction => {
+
+    const notification: Notification = {
+        id: '' + Date.now(),
+        msg,
+        type
+    }
+
     return {
         type: GlobalActionTypes.PushNotification,
-        payload: data
+        payload: notification
     }
 }
