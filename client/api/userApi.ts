95daec1683055d642dd74e8bf5fb6c3af36b4ef1
Quazar8 <barcode12d@gmail.com>
2021-04-01 23:29:55 +0300

    Changing the types in the login form component

--- client/api/userApi.ts
+++ client/api/userApi.ts
@@ -2,9 +2,7 @@ import { baseUrl, postQueryOptions } from './reqOptions'
 import { LoginEntryObj, UserLoggedObj } from '../../server/routes/user/userTypes'
 import { ServerResponse } from '../../server/responses'
 
-export type LoginResponse = Promise<ServerResponse<UserLoggedObj>>
-
-export const loginServer = async (data: LoginEntryObj): LoginResponse => {
+export const loginServer = async (data: LoginEntryObj): Promise<ServerResponse<UserLoggedObj>> => {
     const res = await fetch(baseUrl + '/user/login', postQueryOptions(data))
     return res.json()
 }
--- client/components/user-forms/LoginForm.tsx
+++ client/components/user-forms/LoginForm.tsx
@@ -3,11 +3,15 @@ import { loginServer } from '../../api/userApi'
 import { connect, MapDispatchFn } from '../../store/connect'
 import { addNotification, Notification } from '../../store/globalActions'
 
-const LoginFormView = () => {
+type DispProps = {
+    pushNotification: (type: Notification['type'], msg: Notification['msg']) => void
+}
+
+const LoginFormView = ({ pushNotification }: DispProps) => {
     const usernameRef = useRef<HTMLInputElement>()
     const passwordRef = useRef<HTMLInputElement>()
 
-    const loginUser = (e: FormEvent | null) => {
+    const loginUser = async (e: FormEvent | null) => {
         e?.preventDefault()
 
         const data = {
@@ -19,9 +23,9 @@ const LoginFormView = () => {
             return
         }
 
-        loginServer(data).then(resp => {
-            console.log(resp)
-        })
+        const resp = await loginServer(data)
+
+        console.log(resp)
     }
 
     const submitOnEnter = (e: KeyboardEvent) => {
@@ -49,9 +53,9 @@ const LoginFormView = () => {
     )
 }
 
-const mapDispatch: MapDispatchFn = (dispatch) => ({
-    pushNotification: (data: Notification) => {
-        dispatch(addNotification(data))
+const mapDispatch: MapDispatchFn<DispProps> = (dispatch) => ({
+    pushNotification: (type, msg) => {
+        dispatch(addNotification(type, msg))
     }
 })
 
