e9469d3e5bf4e5697c72830a7ad88b46f8b998af
Quazar8 <barcode12d@gmail.com>
2021-04-06 23:33:10 +0300

    Creating a test server route which is available only in development

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,9 +1,16 @@
 import { Application, Request, Response } from 'express'
 import path from 'path'
 import enableUserRoutes from './user/UserRoutes.js'
+import { successResponse } from '../responses.js'
 
 const establishRouteEndpoints = (app: Application): void => {
     enableUserRoutes(app)
+
+    if (process.env.NODE_ENV?.trim() === 'development') {
+        app.post('/test', (req: Request, res: Response) => {
+            res.send(successResponse({}, 'Test endpoint'))
+        })
+    }
 
     app.get('*', (req: Request, res: Response) => {
         res.sendFile(path.resolve('./build/client/index.html'))
