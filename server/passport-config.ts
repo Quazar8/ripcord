19b89510fface9e20d77b945a88084b75db4e0c8
Quazar8 <barcode12d@gmail.com>
2021-04-08 23:40:26 +0300

    Passing UserInfo type object in the JWT strategy

--- server/passport-config.ts
+++ server/passport-config.ts
@@ -2,6 +2,7 @@ import { Error } from 'mongoose'
 import passport from 'passport'
 import passportJWT, { StrategyOptions } from 'passport-jwt'
 import { IUserModel, User } from './db/models.js'
+import { UserInfo } from './routes/user/userTypes'
 import { jwtKey } from './global-vars.js'
 
 const { Strategy, ExtractJwt } = passportJWT
@@ -16,7 +17,14 @@ const configurePassport = (): void => {
         User.findOne({ _id: payload.id }, (err: Error, user: IUserModel) => {
             if (err) return done(err, false)
 
-            if (user) return done(null, user)
+            if (user) {
+                const reqUser: UserInfo = {
+                    username: user.username,
+                    registeredAt: user.registeredAt
+                }
+                
+                return done(null, reqUser)
+            }
             else return done(null, false)
         })
     }))
