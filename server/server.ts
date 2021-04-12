b69264fed2a75dcdc5de68bd746dc63b2e892df5
Quazar8 <barcode12d@gmail.com>
2021-04-12 23:28:51 +0300

    Removing socket.io from the project

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,6 @@
 import http from 'http'
 import express, { Application } from 'express'
 import passport from 'passport'
-import * as SocketIO from 'socket.io'
 import { connectToDb } from './db/db.js'
 import configurePassport from './passport-config.js'
 import { errorHandler } from './middlewares.js'
@@ -28,12 +27,6 @@ initializeWebpack(app)
 establishRouteEndpoints(app)
 
 const server = http.createServer(app)
-const SIO = SocketIO.listen
-console.log(SIO)
-
-// io.onconnection((socket: SocketIO.Server) => {
-//     console.log('socket connected')
-// })
 
 server.listen(PORT, () => {
     console.log('\x1b[33m%s\x1b[0m',`Server listening at http://localhost:${PORT}`)
