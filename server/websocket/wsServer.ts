71faf0be33d97ee987d43649f91337feaec94865
Quazar8 <barcode12d@gmail.com>
2021-04-15 23:58:47 +0300

    Workign on the ws authentication

--- server/websocket/wsServer.ts
+++ server/websocket/wsServer.ts
@@ -1,5 +1,6 @@
 import ws from 'ws'
 import { Server } from 'http'
+import passport from 'passport'
 
 export const websocketServer = (server: Server) => {
     const socketServer = new ws.Server({
@@ -10,7 +11,11 @@ export const websocketServer = (server: Server) => {
         console.log('ws server is listening')
     })
 
-    socketServer.on('connection', (socket) => {
+    socketServer.on('connection', (socket, req) => {
+        console.log('ws cookie', req.headers.cookie)
+        passport.authenticate('jwt', (err, user) => {
+            console.log('ws user', err)
+        })(req)
         socket.on('message', (msg) => {
             console.log('received message', msg)
         })
