f9e9adb79c302cac0e380f946fe2eed1f85aafc0
Quazar8 <barcode12d@gmail.com>
2021-04-17 16:49:02 +0300

    Adding parsed cookies to the websocket request

--- server/websocket/wsServer.ts
+++ server/websocket/wsServer.ts
@@ -1,9 +1,10 @@
 import ws from 'ws'
-import { Server } from 'http'
+import {Server, IncomingMessage} from 'http'
 import passport from 'passport'
+import { getCookies, Cookies } from '../utils.js'
 
-const cookieReader = () => {
-    throw new Error('not incorporated')
+type IncMessWCookies = IncomingMessage & {
+    cookies: Cookies
 }
 
 export const websocketServer = (server: Server) => {
@@ -15,8 +16,10 @@ export const websocketServer = (server: Server) => {
         console.log('ws server is listening')
     })
 
-    socketServer.on('connection', (socket, req) => {
+    socketServer.on('connection', (socket, req: IncMessWCookies) => {
         console.log('ws cookie', req.headers.cookie)
+        console.log('ws parsed cookie', getCookies(req.headers.cookie))
+        req.cookies = getCookies(req.headers.cookie)
         passport.authenticate('jwt', (err, user) => {
             console.log('ws user', user)
         })(req)
