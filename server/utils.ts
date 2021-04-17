f96dfe8c146cdeb6594775c8af7efa231dfe42b9
Quazar8 <barcode12d@gmail.com>
2021-04-17 16:23:06 +0300

    Exporting a function for creating cookies object from the cookie request header

--- server/utils.ts
+++ server/utils.ts
@@ -1,3 +1,18 @@
 export const isDev = (): boolean => {
     return process.env.NODE_ENV?.trim() === 'development'
 }
+
+export type Cookies = {
+    [key:string]: string
+}
+
+export const getCookies = (headerCookie: string): Cookies => {
+    let cookieArr = headerCookie.split(', ')
+    let cookies: Cookies = {}
+    for (let strCookie of cookieArr) {
+        let [name, val] = strCookie.split('=')
+        cookies[name] = val
+    }
+
+    return cookies
+}
