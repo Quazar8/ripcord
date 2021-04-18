f76e6d8875e4b8da818c85da89a882901b5b3658
Quazar8 <barcode12d@gmail.com>
2021-04-18 18:52:06 +0300

    Connecting HomeScreen component to the store

--- client/components/home/HomeScreen.tsx
+++ client/components/home/HomeScreen.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { MapStateFn, connect } from '../../store/store'
+import { UserState } from '../../store/globalReducer'
 
 import ChatDisplay from './ChatDisplay'
 
-const HomeScreen = () => {
+type StateProps = {
+    user: UserState
+}
+
+const HomeScreenView = () => {
     return (
         <section className = "home-screen">
             <ChatDisplay />            
         </section>
     )
 }
+
+const mapState: MapStateFn<StateProps> = state => ({
+    user: state.global.user
+})
+
+const HomeScreen = connect(mapState, null)(HomeScreenView)
 
 export default HomeScreen
