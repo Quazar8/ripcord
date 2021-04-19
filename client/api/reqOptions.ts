12f502de3613e0a16f9e8a5ec4dcc1d3b12fe61d
Quazar8 <barcode12d@gmail.com>
2021-04-19 22:42:55 +0300

    Client side get request options creator method

--- client/api/reqOptions.ts
+++ client/api/reqOptions.ts
@@ -8,3 +8,15 @@ export const postQueryOptions = (data: Object): RequestInit => ({
     body: JSON.stringify(data),
     credentials: 'include'
 })
+
+export const getQueryOptions = (includeCredentials: boolean = false): RequestInit => {
+    const options: RequestInit = {
+        method: 'GET'
+    }
+
+    if (includeCredentials) {
+        options.credentials = 'include'
+    }
+
+    return options
+}
