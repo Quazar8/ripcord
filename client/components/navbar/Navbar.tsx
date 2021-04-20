66fdff08fb4431ff6d67f4ff0529eafc95dbe524
Quazar8 <barcode12d@gmail.com>
2021-04-20 23:31:59 +0300

    Connecting the navbar to the store

--- client/components/navbar/Navbar.tsx
+++ client/components/navbar/Navbar.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import NavLinks from './NavLinks'
 
-const Navbar = () => {
+import { connect, MapStateFn } from '../../store/store'
+import { UserState } from '../../store/globalReducer'
+
+type StateProps = {
+    user: UserState
+}
+
+const NavbarView = () => {
     return (
         <nav className = "main-navbar">
             <NavLinks />
         </nav>
     )
 }
+
+const mapState: MapStateFn<StateProps> = (state) => ({
+    user: state.global.user
+})
+
+const Navbar = connect(mapState, null)(NavbarView)
 
 export default Navbar
