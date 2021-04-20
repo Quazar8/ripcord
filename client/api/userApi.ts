6b304d9c6516b78597cfa9111430a81a98964ac6
Quazar8 <barcode12d@gmail.com>
2021-04-20 22:38:26 +0300

    Client side method for retrieving user info when token is present

--- client/api/userApi.ts
+++ client/api/userApi.ts
@@ -1,8 +1,12 @@
-import { baseUrl, postQueryOptions } from './reqOptions'
+import { baseUrl, postQueryOptions, getQueryOptions } from './reqOptions'
 import { LoginEntryObj, UserLoggedObj } from '../../server/routes/user/userTypes'
 import { ServerResponse } from '../../server/responses'
 
 export const loginServer = async (data: LoginEntryObj): Promise<ServerResponse<UserLoggedObj>> => {
     const res = await fetch(baseUrl + '/user/login', postQueryOptions(data))
     return res.json()
+}
+
+export const getUseInfoIfLogged = async (): Promise<ServerResponse<UserLoggedObj>> => {
+    return (await fetch(baseUrl + '/user/loggedUserInfo', getQueryOptions(true))).json()
 }
