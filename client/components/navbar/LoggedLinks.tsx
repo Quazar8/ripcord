9da07882019b292579240e531304a8023a098290
Quazar8 <barcode12d@gmail.com>
2021-04-21 16:01:06 +0300

    Connecting the logged links to the store

--- client/components/navbar/LoggedLinks.tsx
+++ client/components/navbar/LoggedLinks.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
-const LoggedLinks = () => {
+import { logoutUser } from '../../api/userApi'
+import { connect, MapDispatchFn } from '../../store/store'
+import { removeUserInfoAction } from '../../store/globalActions'
+
+type DispProps = {
+    removeUserFromState: () => void
+}
+
+const LoggedLinksView = () => {
+    const logoutHandler = async () => {
+        const res = await logoutUser()
+        
+        if (res.error) {
+            return
+        }
+
+
+    }
+
     return (
         <div className = "logged-links-container">
             <button>Log out</button>
         </div>
     )
 }
+
+const mapDisp: MapDispatchFn<DispProps> = (dispatch) => ({
+    removeUserFromState: () => {
+        dispatch(removeUserInfoAction())
+    }
+})
+
+const LoggedLinks = connect(null, mapDisp)(LoggedLinksView)
 
 export default LoggedLinks
