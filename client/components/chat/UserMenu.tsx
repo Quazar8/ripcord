d8b6dcc316f4bc43a98ce59d422b065d3c4b9987
Quazar8 <barcode12d@gmail.com>
2021-04-24 17:08:28 +0300

    Adding a menu bar button to the user menu component

--- client/components/chat/UserMenu.tsx
+++ client/components/chat/UserMenu.tsx
@@ -5,10 +5,13 @@ type Props = {
 }
 
 const UserMenu = ({ showUserMenu }: Props) => {
-    if (showUserMenu) return null
-    
+    if (!showUserMenu) return null
+
     return (
         <section className = "user-menu">
+            <div className = "menu-bar">
+                <button>X</button>
+            </div>
             User Menu Component
         </section>
     )
