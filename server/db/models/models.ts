fa81289acfb023ebec69e7baafffdca5d3fc65f7
Quazar8 <barcode12d@gmail.com>
2021-04-27 17:23:13 +0300

    Change of the freidnsIds type

--- server/db/models/models.ts
+++ server/db/models/models.ts
@@ -1,18 +1,18 @@
-import mongoose, { Document } from 'mongoose'
+import mongoose, { Document, Types } from 'mongoose'
 const { Schema, model } = mongoose
 
 const UserSchema = new Schema ({
     username: { type: String, unique: true },
     password: String,
     registeredAt: { type: Date, default: () => Date.now() },
-    friendsIds: { type: [String], default: [] }
+    friendsIds: { type: [Schema.Types.ObjectId], default: [] }
 })
 
 export interface IUserModel {
     username: string,
     password: string,
     registeredAt: Date,
-    friendsIds: string[]
+    friendsIds: Types.ObjectId[]
 }
 
 export type IUserDoc = Document & IUserModel
