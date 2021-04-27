4afcccbd103468de9437dadd36edb4aa8362281a
Quazar8 <barcode12d@gmail.com>
2021-04-27 19:11:57 +0300

    Initial handle fo the register form submit

--- client/components/user-forms/RegisterForm.tsx
+++ client/components/user-forms/RegisterForm.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { FormEvent, useRef } from 'react'
+
+// import { registerUser } from '../../api/userApi'
 
 const RegisterForm = () => {
+    const usernameRef = useRef<HTMLInputElement>()
+    const passwordRef = useRef<HTMLInputElement>()
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+        
+        const data = {
+            username: usernameRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        if (!data.username || !data.password) {
+            return
+        }
+    }
+
     return (
-        <form className = "user-form">
+        <form className = "user-form" onSubmit = { handleSubmit }>
             <label>
                 Username:
-                <input type = "text" />
+                <input ref = { usernameRef } type = "text" />
             </label>
             <label>
                 Password:
-                <input type = "password" />
+                <input ref = { passwordRef } type = "password" />
             </label>
             <label>
                 Confirm Password:
