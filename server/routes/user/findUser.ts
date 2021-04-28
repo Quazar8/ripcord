7ea3717b6b53d6c0cde52daf7bab9808637f7b3f
Quazar8 <barcode12d@gmail.com>
2021-04-28 14:52:20 +0300

    A find friend request to the server results in appropriate server response

--- server/routes/user/findUser.ts
+++ server/routes/user/findUser.ts
@@ -1,6 +1,31 @@
 import { Request, Response } from "express";
-import { successResponse } from '../../responses.js'
+import { successResponse, errorResponse } from '../../responses.js'
+import { IUserDoc, User } from '../../db/models/models.js'
+import { UserInfo } from "./userTypes.js";
+import { Document } from "mongoose";
 
-export const findFriend = (req: Request, res: Response) => {
-    res.send(successResponse({ query: req.query }, 'find user endpoint'))
+const isUserDoc = (doc: Document): doc is IUserDoc => {
+    return doc?._id
+}
+
+export const findFriend = async (req: Request, res: Response) => {
+    const { username } = req.query
+    if (!username) {
+        res.status(400).send(errorResponse('Bad request'))
+        return
+    }
+
+    const found = await User.findOne({ username })
+    if (isUserDoc(found)) {
+        const userInfo: UserInfo = {
+            id: found._id,
+            username: found.username,
+            registeredAt: found.registeredAt,
+            friendsIds: found.friendsIds
+        }
+
+        res.send(successResponse({ found: userInfo }, ''))
+    } else {
+        res.send(successResponse({}, 'No user found'))
+    }
 }
