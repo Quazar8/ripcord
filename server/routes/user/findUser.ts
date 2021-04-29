3f88c8ca9aa267ec66f9fe1f4900ea48e815e9f2
Quazar8 <barcode12d@gmail.com>
2021-04-29 23:59:40 +0300

    Updating  userinc friend requests when added as a friend

--- server/routes/user/findUser.ts
+++ server/routes/user/findUser.ts
@@ -1,7 +1,10 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { successResponse, errorResponse, ServerResponse } from '../../responses.js'
 import { IUserDoc, User } from '../../db/models/models.js'
+import { } from '../user/UserTypes'
+import { onlineUsers } from '../../websocket/wsServer.js'
 import { Document } from "mongoose"
+import { ReqWUser } from '../../types/RequestTypes'
 
 export type AddFriendRes = ServerResponse<{
     found: boolean,
@@ -12,7 +15,7 @@ const isUserDoc = (doc: Document): doc is IUserDoc => {
     return doc?._id
 }
 
-export const addFriend = async (req: Request, res: Response) => {
+export const addFriend = async (req: ReqWUser, res: Response) => {
     const { username } = req.query
     if (!username) {
         res.status(400).send(errorResponse('Bad request'))
@@ -23,10 +26,18 @@ export const addFriend = async (req: Request, res: Response) => {
     
     let response: AddFriendRes = null
     if (isUserDoc(found)) {
-        response = successResponse({ 
-            found: true,
-            sentRequest: false
-        } , '')
+        found.incFriendRequests.push(req.user.id)
+
+        try {
+            const updated = await found.save()
+            response = successResponse({ 
+                found: true,
+                sentRequest: false
+            }, '')
+        } 
+        catch {
+            response = errorResponse('Something went wrong')
+        }
     } else {
         response = successResponse({
             sentRequest: false,
