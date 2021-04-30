ed2626e5071f52379b74e518b55de0ccb55e8843
Quazar8 <barcode12d@gmail.com>
2021-04-30 18:30:44 +0300

    On websoket error trying to reconnect to server every 2 seconds

--- client/socket/socket.ts
+++ client/socket/socket.ts
@@ -3,7 +3,7 @@ import { pushNotification } from '../store/globalActions'
 export let socket: WebSocket = null
 
 export const establishWS = (dispNotification: ReturnType<typeof pushNotification>) => {
-    if (socket !== null) {
+    if (socket !== null && socket.readyState === socket.OPEN) {
         return
     }
 
@@ -13,11 +13,12 @@ export const establishWS = (dispNotification: ReturnType<typeof pushNotification
         console.log('socket connection is opened')
     }
 
-        dispNotification('error', 'Error connecting to the chat service')
     socket.onerror = (ev) => {
         console.log('Error connecting socket')
         dispNotification('error', 'Error connecting to the chat service')
-        // socket = establishWS()
+        setTimeout(() => {
+            establishWS(dispNotification)
+        }, 2000)
     }
 
     socket.onmessage = (msg) => {
