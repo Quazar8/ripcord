55a49ff24bcbb615578f1357785dd5b502ef1ee0
Quazar8 <barcode12d@gmail.com>
2021-05-03 19:28:14 +0300

    Appendung a nav bar component to the friends window

---
+++ client/components/chat/FriendsWindow/FriendsMenuBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FriendsMenuBar = () => {
+    return (
+        <nav className = "friends-menu-bar">
+            <button>Add</button>
+        </nav>
+    )
+}
+
+export default FriendsMenuBar
--- client/components/chat/FriendsWindow/FriendsWindow.tsx
+++ client/components/chat/FriendsWindow/FriendsWindow.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { pushNotification } from '../../../store/globalActions'
 
 import AddFriend from './AddFriend'
+import FriendsMenuBar from './FriendsMenuBar'
 
 type Props = {
     dispNotification: ReturnType<typeof pushNotification>
@@ -10,6 +11,7 @@ type Props = {
 const FriendsWindow = (props: Props) => {
     return (
         <div className = "friends-window">
+            <FriendsMenuBar />
             <AddFriend 
                 dispNotification = { props.dispNotification }
             />
