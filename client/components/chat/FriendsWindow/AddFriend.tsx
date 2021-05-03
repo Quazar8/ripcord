9c842852a413fe474996dc895995223c90be9286
Quazar8 <barcode12d@gmail.com>
2021-05-03 19:25:46 +0300

    Add friend view to a seperate component

---
+++ client/components/chat/FriendsWindow/AddFriend.tsx
@@ -0,0 +1,47 @@
+import React, { useRef, useState, FormEvent } from 'react'
+import { resHasError } from '../../../api/utils'
+import { addFriend } from '../../../api/userApi'
+import { pushNotification } from '../../../store/globalActions'
+
+type Props = {
+    dispNotification: ReturnType<typeof pushNotification>
+}
+
+const AddFriend = (props: Props) => {
+    const findUserRef = useRef<HTMLInputElement>()
+    const [notiificationMsg, setNotificationMsg] = useState('')
+
+    const handleFindFriendSubmit = async (e: FormEvent) => {
+        e.preventDefault()
+
+        const username = findUserRef.current.value
+        if (!username) return
+
+        const res = await addFriend(username)
+        if (resHasError(res)) {
+            props.dispNotification('error', res.errorMsg)
+        } else if (res.data.sentRequest) {
+            setNotificationMsg('Friend request sent')
+        } else if (!res.data.found) {
+            setNotificationMsg('No such user found')
+        }
+    }
+
+    return (
+        <div>
+            <form className = "add-friend-form" onSubmit = { handleFindFriendSubmit }>
+                <input ref = { findUserRef } type = "search" />
+                <input type = "submit" value = "Add User" />
+            </form>
+            {
+                notiificationMsg 
+                ? <div className = "notification">
+                    { notiificationMsg }
+                  </div>
+                : null
+            }
+        </div>
+    )
+}
+
+export default AddFriend
--- client/components/chat/FriendsWindow/FriendsWindow.tsx
+++ client/components/chat/FriendsWindow/FriendsWindow.tsx
@@ -1,45 +1,18 @@
-import React, { useState, useRef, FormEvent } from 'react'
-import { addFriend } from '../../../api/userApi'
-import { resHasError } from '../../../api/utils'
+import React from 'react'
 import { pushNotification } from '../../../store/globalActions'
+
+import AddFriend from './AddFriend'
 
 type Props = {
     dispNotification: ReturnType<typeof pushNotification>
 }
 
 const FriendsWindow = (props: Props) => {
-    const findUserRef = useRef<HTMLInputElement>()
-    const [notiificationMsg, setNotificationMsg] = useState('')
-
-    const handleFindFriendSubmit = async (e: FormEvent) => {
-        e.preventDefault()
-
-        const username = findUserRef.current.value
-        if (!username) return
-
-        const res = await addFriend(username)
-        if (resHasError(res)) {
-            props.dispNotification('error', res.errorMsg)
-        } else if (res.data.sentRequest) {
-            setNotificationMsg('Friend request sent')
-        } else if (!res.data.found) {
-            setNotificationMsg('No such user found')
-        }
-    }
-
     return (
         <div className = "friends-window">
-            <form className = "add-friend-form" onSubmit = { handleFindFriendSubmit }>
-                <input ref = { findUserRef } type = "search" />
-                <input type = "submit" value = "Add User" />
-            </form>
-            {
-                notiificationMsg 
-                ? <div className = "notification">
-                    { notiificationMsg }
-                  </div>
-                : null
-            }
+            <AddFriend 
+                dispNotification = { props.dispNotification }
+            />
         </div>
     )
 }
