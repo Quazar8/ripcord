6cae3342c1dba35d095013b9a3e245f4c4205573
Quazar8 <barcode12d@gmail.com>
2021-05-08 18:30:30 +0300

    Retrieving friends lsit data in the corresponding view

--- client/components/chat/FriendsWindow/FriendsList.tsx
+++ client/components/chat/FriendsWindow/FriendsList.tsx
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FriendClientInfo } from '../../../../server/types/userTypes'
+ 
+import { retrieveFriends } from '../../../api/userApi'
+import { resHasError } from '../../../api/utils'
 
 const FriendsList = () => {
+    const [friendsList, setFriendsList] = useState<{
+        online: FriendClientInfo[],
+        offline: FriendClientInfo[]
+    }>({
+        online: [],
+        offline: []
+    })
+
+    const fetchFriends = async () => {
+        const res = await retrieveFriends()
+
+        console.log(res)
+        if (resHasError(res)) {
+            return
+        }
+
+        setFriendsList(res.data)
+    }
+
+    useEffect(() => {
+        fetchFriends()
+    }, [])
+
+
     return (
         <div className = "friends-list">
             Friends lsit component
