ded5fdadb9152a5820d6306a6557b0d90c2c4f44
Quazar8 <barcode12d@gmail.com>
2021-05-09 18:27:03 +0300

    Not fisplaying div's when online or offline friends list is empty

--- client/components/chat/FriendsWindow/FriendsList.tsx
+++ client/components/chat/FriendsWindow/FriendsList.tsx
@@ -29,31 +29,46 @@ const FriendsList = () => {
         fetchFriends()
     }, [])
 
+    if (friendsList.online.length === 0 && friendsList.offline.length === 0) {
+        return (
+            <div className = "friends-list">
+                <h3>You don't have friends yet</h3>
+            </div>
+        )
+    }
 
     return (
         <div className = "friends-list">
-            <div>
-                <h2>Online:</h2>
-                {
-                    friendsList.online.map((f, i) => (
-                        <FriendsPlate
-                            friend = { f }
-                            key = { i }
-                        />
-                    ))
-                }
-            </div>
-            <div>
-                <h2>Offline:</h2>
-                {
-                    friendsList.offline.map((f, i) => (
-                        <FriendsPlate
-                            friend = { f }
-                            key = { i }
-                        />
-                    ))
-                }
-            </div>
+            {
+                friendsList.online.length > 0
+                ? <div>
+                    <h2>Online:</h2>
+                    {
+                        friendsList.online.map((f, i) => (
+                            <FriendsPlate
+                                friend = { f }
+                                key = { i }
+                            />
+                        ))
+                    }
+                </div>
+                : null
+            }
+            {
+                friendsList.offline.length > 0
+                ? <div>
+                    <h2>Offline:</h2>
+                    {
+                        friendsList.offline.map((f, i) => (
+                            <FriendsPlate
+                                friend = { f }
+                                key = { i }
+                            />
+                        ))
+                    }
+                </div>
+                : null
+            }
         </div>
     )
 }
