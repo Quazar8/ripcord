2a1db2e4668e2468ae4df26559a077bb0707a128
Quazar8 <barcode12d@gmail.com>
2021-05-11 16:42:32 +0300

    Adjusting the channel  mongoose model

--- server/db/models/channel.ts
+++ server/db/models/channel.ts
@@ -3,17 +3,17 @@ import { Message } from '../../types/ChatTypes'
 const { Schema, model } = mongoose
 
 export interface IChannel {
-    participantOne: Types.ObjectId,
-    participantTwo: Types.ObjectId,
-    createdAt: Date,
+    participantOne: Types.ObjectId
+    participantTwo: Types.ObjectId
+    createdAt: Date
     messages: Message[]
 }
 
-const channelSchema = new Schema({
+const channelSchema = new Schema<IChannel>({
     participantOne: Schema.Types.ObjectId,
     participantTwo: Schema.Types.ObjectId,
-    createdAt: { type: Date, default: new Date()},
-    messages: Array<Message>
+    createdAt: { type: Date, default: new Date(Date.now()) },
+    messages: { type: Array, default: [] }
 })
 
 export const Channel = model('Channel', channelSchema)
