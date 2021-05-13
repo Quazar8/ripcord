1a017ddf874a2db9438412b344d66a6be0f40c17
Quazar8 <barcode12d@gmail.com>
2021-05-13 21:59:32 +0300

    Validating the chat message sent to server

--- server/websocket/messageHandler.ts
+++ server/websocket/messageHandler.ts
@@ -1,9 +1,30 @@
+import { Channel } from "../db/models/channel.js";
 import { IUserDoc } from "../db/models/user.js";
-import { ChatMessagePayload } from "../types/ChatTypes.js";
+import { ChatMessagePayload, isChannelDoc } from "../types/ChatTypes.js";
 import { WSDataType, WSMessage } from "../types/WebsocketTypes.js";
 
-const handleChatMessage = (payload: ChatMessagePayload, byUser: IUserDoc) => {
-    console.log(payload)
+const handleChatMessage = async (payload: ChatMessagePayload, byUser: IUserDoc) => {
+    if (!payload.channelId || !payload.content || !byUser) {
+        return
+    }
+
+    try {
+        const channel = await Channel.findById(payload.channelId)
+
+        if (!isChannelDoc(channel)) {
+            return
+        }
+
+        if (!byUser._id.equals(channel.participantTwo)
+            && byUser._id.equals(channel.participantTwo)) {
+            return
+        }
+
+        console.log('chat message', payload)
+    } 
+    catch (err) {
+        console.error(err)
+    }
 }
 
 const messageHandler = (wsMsg: WSMessage<any>, user: IUserDoc) => {
