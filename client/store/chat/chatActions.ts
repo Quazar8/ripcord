542cb09e21813799dd525fef72616d833233d952
Quazar8 <barcode12d@gmail.com>
2021-05-17 23:18:55 +0300

    Change chat recipient action creator

--- client/store/chat/chatActions.ts
+++ client/store/chat/chatActions.ts
@@ -1,7 +1,13 @@
 import { Action } from "../storeComponents/StoreTypes";
 
 export enum ChatActionTypes {
-    CHANGE_CHAT_RECIPIENT
+    CHANGE_CHAT_RECIPIENT,
+    UPDATE_ACTIVE_CHANNELS
 }
 
 export type ChatAction = Action<ChatActionTypes, any>
+
+export const changeCHatRecipientAction = (recipientId: string): ChatAction => ({
+    type: ChatActionTypes.CHANGE_CHAT_RECIPIENT,
+    payload: recipientId
+})
