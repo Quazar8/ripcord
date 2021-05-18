d318c72ec3f5d69ae7e4636e30214dfae8494d2c
Quazar8 <barcode12d@gmail.com>
2021-05-18 23:26:40 +0300

    Channel id in the chat reducer and actions

--- client/store/chat/chatActions.ts
+++ client/store/chat/chatActions.ts
@@ -3,7 +3,8 @@ import { Action } from "../storeComponents/StoreTypes";
 
 export enum ChatActionTypes {
     CHANGE_CHAT_RECIPIENT = 'CHANGE_RECIPIENT_ID',
-    UPDATE_ACTIVE_CHANNELS = "UPDATE_ACTIVE_CHANNELS"
+    UPDATE_ACTIVE_CHANNELS = "UPDATE_ACTIVE_CHANNELS",
+    CHANGE_CHANNEL_ID = "CHANGE_CHANNEL_ID"
 }
 
 export type ChatAction = Action<ChatActionTypes, any>
@@ -16,4 +17,9 @@ export const changeCHatRecipientAction = (recipientId: string): ChatAction => ({
 export const updateActiveChannelsAction = (channels: ActiveChannelInfo[]): ChatAction => ({
     type: ChatActionTypes.UPDATE_ACTIVE_CHANNELS,
     payload: channels
+})
+
+export const changeChannelIdAction = (channelId: string): ChatAction => ({
+    type: ChatActionTypes.CHANGE_CHANNEL_ID,
+    payload: channelId
 })
--- client/store/chat/chatReducer.ts
+++ client/store/chat/chatReducer.ts
@@ -3,12 +3,14 @@ import { ChatAction, ChatActionTypes } from "./chatActions";
 
 export type ChatState = {
     currentRecipientId: string,
-    activeChannels: ActiveChannelInfo[]
+    activeChannels: ActiveChannelInfo[],
+    currentChannelId: string
 }
 
 export const chatStateInit: ChatState = {
     currentRecipientId: '',
-    activeChannels: []
+    activeChannels: [],
+    currentChannelId: ''
 }
 
 const changeRecipient = (state: ChatState, recipientId: string): ChatState => {
@@ -25,12 +27,21 @@ const updateActiveChannels = (state: ChatState, channels: ActiveChannelInfo[]): 
     }
 }
 
+const changeChannelId = (state: ChatState, channelId: string): ChatState => {
+    return {
+        ...state,
+        currentChannelId: channelId
+    }
+}
+
 export const chatReducer = (state: ChatState = chatStateInit, action: ChatAction): ChatState => {
     switch (action.type) {
         case ChatActionTypes.CHANGE_CHAT_RECIPIENT:
             return changeRecipient(state, action.payload)
         case ChatActionTypes.UPDATE_ACTIVE_CHANNELS:
             return updateActiveChannels(state, action.payload)
+        case ChatActionTypes.CHANGE_CHANNEL_ID:
+            return changeChannelId(state, action.payload)
         default: return state
     }
 }
