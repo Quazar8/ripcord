e63dd0452e8869f860814abdce1a01107740568c
Quazar8 <barcode12d@gmail.com>
2021-05-18 23:18:58 +0300

    Getting channel info from server with channel id provided

--- server/routes/chat/chatChannelInfo.ts
+++ server/routes/chat/chatChannelInfo.ts
@@ -14,6 +14,36 @@ type ChatChannelInfoData = {
 
 export type ChatChannelInfoRes = ServerResponse<ChatChannelInfoData>
 
+
+const genChatChannelInfoData = (channel: ChannelDoc, recipient: UserDoc): ChatChannelInfoData => {
+    const messages: MessageClient[] = channel.messages.map(m => {
+        return {
+            date: m.date,
+            edited: m.edited,
+            authorId: m.authorId.toHexString(),
+            content: m.content
+        }
+    }) 
+
+    const channelInfo: ChannelClientInfo = {
+        id: channel._id,
+        messages,
+        participantOne: channel.participantOne.toHexString(),
+        participantTwo: channel.participantTwo.toHexString()
+    }
+
+    const recipientInfo: RecipientInfo = {
+        id: recipient._id.toHexString(),
+        username: recipient.username,
+        status: isOnline(recipient._id) ? UserStatus.Online : UserStatus.Offline
+    }
+
+    return {
+        recipient: recipientInfo,
+        channel: channelInfo
+    }
+}
+
 export const chatChannelInfoHandler = async (req: ReqWUser, res: Response) => {
     let response: ChatChannelInfoRes = null
     let status: number = 200
@@ -37,34 +67,6 @@ export const chatChannelInfoHandler = async (req: ReqWUser, res: Response) => {
         return
     }
 
-    const responseData = (channel: ChannelDoc, recipient: UserDoc): ChatChannelInfoData => {
-        const messages: MessageClient[] = channel.messages.map(m => {
-            return {
-                date: m.date,
-                edited: m.edited,
-                authorId: m.authorId.toHexString(),
-                content: m.content
-            }
-        }) 
-
-        const channelInfo: ChannelClientInfo = {
-            id: channel._id,
-            messages,
-            participantOne: channel.participantOne.toHexString(),
-            participantTwo: channel.participantTwo.toHexString()
-        }
-
-        const recipientInfo: RecipientInfo = {
-            id: recipient._id.toHexString(),
-            username: recipient.username,
-            status: isOnline(recipient._id) ? UserStatus.Online : UserStatus.Offline
-        }
-
-        return {
-            recipient: recipientInfo,
-            channel: channelInfo
-        }
-    }
 
     const handleRequest = async (requester: UserDoc, recipient: UserDoc) => {
         if (!requester.channels) {
@@ -96,7 +98,7 @@ export const chatChannelInfoHandler = async (req: ReqWUser, res: Response) => {
         
         const channel = await Channel.findById(channelId)
         if (isChannelDoc(channel)) {
-            return successResponse(responseData(channel, recipient))
+            return successResponse(genChatChannelInfoData(channel, recipient))
         }
 
         return errorResponse('Couldn\'t get channel info')
@@ -120,6 +122,48 @@ export const chatChannelInfoHandler = async (req: ReqWUser, res: Response) => {
     res.status(status).send(response)
 }
 
-export const getActiveChannelInfo = (req: ReqWUser, res: Response) => {
-    res.send(successResponse({}))
+export const getActiveChannelInfo = async (req: ReqWUser, res: Response) => {
+    let response: ChatChannelInfoRes = null
+    let status: number = 200
+
+    if (!req.user) {
+        response = errorResponse('No user provided')
+        res.status(400).send(response)
+        return
+    }
+
+    const { channelId } = req.params
+    if (!channelId) {
+        response = errorResponse('Channel id is missing')
+        res.status(400).send(response)
+        return
+    }
+
+    try {
+        const channel = await Channel.findById(channelId)
+        if (!isChannelDoc(channel)) {
+            response = errorResponse('Channel id is incorrect')
+            status = 400
+            return
+        }
+
+        let recipientId = channel.participantOne
+        if (req.user._id.equals(channel.participantOne)) {
+            recipientId = channel.participantTwo
+        }
+
+        const recipient = await User.findById(recipientId)
+        if (!isUserDoc(recipient)) {
+            response = errorResponse('Cannot find the chat participant')
+            return
+        }
+
+        response = successResponse(genChatChannelInfoData(channel, recipient))
+    }
+    catch (err) {
+        response = errorResponse('Something went wrong')
+        status = 500
+    }
+
+    res.status(status).send(response)
 }
