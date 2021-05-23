1b6cc3f0b457ea57a853dc9311c18f9a5ac25eac
Quazar8 <barcode12d@gmail.com>
2021-05-23 15:19:50 +0300

    Changing message class according to its delivery status

--- client/components/chat/ChatMessage.tsx
+++ client/components/chat/ChatMessage.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
-import { MessageClient } from '../../../server/types/ChatTypes'
+import { PendingMsg, PendingMsgStatus } from '../../types/ChatClientTypes'
 
 type Props = {
-    message: MessageClient
+    message: PendingMsg
     authorName: string
 }
 
 export const ChatMessage = ({ message, authorName }: Props) => {
+    let auxClass = 'delivered'
+    if (message.status) {
+        if (message.status === PendingMsgStatus.Pending) {
+            auxClass = 'pending'
+        } else if (message.status === PendingMsgStatus.Failed) {
+            auxClass = 'failed'
+        }
+    }
+
     return (
-        <div className = "message">
+        <div className = { "message" + " " + auxClass }>
             <h4>{ authorName }:</h4>
             <p>{ message.content }</p>
         </div>
