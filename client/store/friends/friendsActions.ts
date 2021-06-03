4a993475cbbe88b5f1f9cf4989fa52f2379b58ea
Quazar8 <barcode12d@gmail.com>
2021-06-03 22:32:01 +0300

    Action creator for removing a pending friend request from store

--- client/store/friends/friendsActions.ts
+++ client/store/friends/friendsActions.ts
@@ -1,12 +1,19 @@
 import { Action } from "../storeComponents/StoreTypes"
+import { FriendRequestsState } from "./friendsReducer"
 
 export enum FriendsActionTypes {
-    FILL_PENDING_REQUESTS = "FILL_PENDING_REQUESTS" 
+    FILL_PENDING_REQUESTS = "FILL_PENDING_REQUESTS",
+    REMOVE_PENDING_REQUEST = "REMOVE_PENDING_REQUEST"
 }
 
 export type FriendsAction = Action<FriendsActionTypes, any>
 
-export const fillPendingRequestsAction = (pendingRequests: any): FriendsAction => ({
+export const fillPendingRequestsAction = (pendingRequests: FriendRequestsState): FriendsAction => ({
     type: FriendsActionTypes.FILL_PENDING_REQUESTS,
     payload: pendingRequests
+})
+
+export const removeFriendRequest = (index: number): FriendsAction => ({
+    type: FriendsActionTypes.REMOVE_PENDING_REQUEST,
+    payload: index
 })
