6ffc253ea45d4467025509bf294e560a0db1c2ab
Quazar8 <barcode12d@gmail.com>
2021-06-29 23:47:58 +0300

    The friend plate dot menu button to appear only on hover

--- client/components/chat/RightWindow/FriendsWindow/FriendPlate.tsx
+++ client/components/chat/RightWindow/FriendsWindow/FriendPlate.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { FriendClientInfo, UserStatus } from '../../../../../server/types/UserTypes'
 import ProfilePic from '../../../user/ProfilePic'
 import { RightWindowContext } from '../RightWindow'
@@ -9,6 +9,7 @@ type Props = {
 }
 
 const FriendPlate = ({ friend }: Props) => {
+    const [RightElement, setRightElement] = useState<JSX.Element>(null)
     let appendClass = friend.status === UserStatus.Online
                     ? 'online' : 'offline'
 
@@ -18,18 +19,36 @@ const FriendPlate = ({ friend }: Props) => {
         toggleChatWRecipientId(friend.id)
     }
 
+    const setDotMenu = () => {
+        setRightElement(<FriendDotMenu friendId = { friend.id } />)
+    }
+
+    const StatusDiv = () => (
+        <div className = "status"></div>
+    )
+
+    const removeDotMenu = () => {
+        if (friend.status === UserStatus.Offline) {
+            setRightElement(null)
+            return
+        }
+
+        setRightElement(<StatusDiv />)
+    }
+
+    useEffect(() => {
+        if (friend.status === UserStatus.Online) {
+            setRightElement(<StatusDiv />)
+        }
+    }, [])
+
     return (
-        <div onClick = { openChatWUser } className = {"friend-plate" + ' ' + appendClass}>
+        <div onMouseEnter = { setDotMenu } onMouseLeave = { removeDotMenu } onClick = { openChatWUser } className = {"friend-plate" + ' ' + appendClass}>
             <div className="friend-info">
                 <ProfilePic picNameOrJson = { friend.profilePic } />
                 <h3>{ friend.username }</h3>
             </div>
-            {
-                friend.status === 'Online'
-                ? <div className = "status"></div>
-                : null
-            }
-            <FriendDotMenu friendId = { friend.id }/>
+            { RightElement }
         </div>
     )
 }
