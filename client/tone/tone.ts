fa36aaf45d8c6db08412c3a63f2e32905b5e598b
Quazar8 <barcode12d@gmail.com>
2021-07-02 18:59:51 +0300

    Adjusting the volume of the notification sound

--- client/tone/tone.ts
+++ client/tone/tone.ts
@@ -3,5 +3,6 @@ import { Synth } from 'tone'
 const synth = new Synth().toDestination()
 
 export const triggerMsgSound = () => {
+    synth.volume.value = -3
     synth.triggerAttackRelease('D6', '32n')
 }
