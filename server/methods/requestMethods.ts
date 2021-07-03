ae427ad9c1872df1d2758e27545d5c33eeaf3b6d
Quazar8 <barcode12d@gmail.com>
2021-07-03 23:41:41 +0300

    Extracting the getCookies function to a different file dedicated to request methods

---
+++ server/methods/requestMethods.ts
@@ -0,0 +1,22 @@
+export type Cookies = {
+    [key:string]: string
+}
+
+export const getCookies = (headerCookie: string): Cookies => {
+    if (!headerCookie) return {}
+
+    const cookieArr = headerCookie.split('; ')
+    const cookies: Cookies = {}
+    for (let strCookie of cookieArr) {
+        const [name, val] = strCookie.split('=')
+
+        if (!val) {
+            console.log('Get Cookeis Method: Cookie in wrong format')
+            continue
+        }
+        
+        cookies[name] = val
+    }
+
+    return cookies
+}
--- server/methods/utils.ts
+++ server/methods/utils.ts
@@ -7,29 +7,6 @@ export const isDev = (): boolean => {
     return process.env.NODE_ENV?.trim() === 'development'
 }
 
-export type Cookies = {
-    [key:string]: string
-}
-
-export const getCookies = (headerCookie: string): Cookies => {
-    if (!headerCookie) return {}
-
-    const cookieArr = headerCookie.split('; ')
-    const cookies: Cookies = {}
-    for (let strCookie of cookieArr) {
-        const [name, val] = strCookie.split('=')
-
-        if (!val) {
-            console.log('Get Cookeis Method: Cookie in wrong format')
-            continue
-        }
-        
-        cookies[name] = val
-    }
-
-    return cookies
-}
-
 export const tryCatchWrapper = (fn: CallableFunction): boolean => {
     try {
         fn()
