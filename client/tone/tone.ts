4de3ce496d2e4106c5a2e68ae40be375874a5066
Quazar8 <barcode12d@gmail.com>
2021-07-03 22:00:43 +0300

    Encapsulating the message signal logic

--- client/tone/tone.ts
+++ client/tone/tone.ts
@@ -1,12 +1,24 @@
 import * as Tone from 'tone'
 
-const synth = new Tone.DuoSynth().toDestination()
-const reverb = new Tone.Reverb(0.4).toDestination()
-synth.connect(reverb)
+const messageSignalInit = () => {
+    const synth = new Tone.DuoSynth().toDestination()
+    const reverb = new Tone.Reverb(0.4).toDestination()
+    synth.connect(reverb)
+    synth.volume.value = -12
+
+    const play = () => {
+        const now = Tone.now()
+        synth.triggerAttackRelease('A4', '32n', now)
+        synth.triggerAttackRelease('D5', '32n', now + 0.15)
+    }
+
+    return {
+        play
+    }
+}
+
+const messageSignal = messageSignalInit()
 
 export const triggerMsgSound = () => {
-    synth.volume.value = -12
-    const now = Tone.now()
-    synth.triggerAttackRelease('A4', '32n', now)
-    synth.triggerAttackRelease('D5', '32n', now + 0.15)
+    messageSignal.play()
 }
