4f7a11d0ebd93eabd5bf9b18603f8f5502454201
Quazar8 <barcode12d@gmail.com>
2021-07-07 14:55:57 +0300

    Inital handling of local media stream errors

--- client/components/chat/RightWindow/callHandler.ts
+++ client/components/chat/RightWindow/callHandler.ts
@@ -9,6 +9,18 @@ type StartCallArgs = {
     isVideoCall: boolean
 }
 
+const userMediaErrorHandler = (e: Error) => {
+    switch (e.name) {
+        case "NotFoundError": {
+            console.log('No mic or camera being found')
+            break;
+        }
+        case "SecurityError":
+        case "PermissDeniedError": break;
+        default: console.log('Unable to open the camera/mic ' + e.message)
+    }
+}
+
 export const startCall = async (args: StartCallArgs) => {
     if (peerConnection) {
         console.error('Already in a call')
@@ -17,6 +29,9 @@ export const startCall = async (args: StartCallArgs) => {
 
     peerConnection = new RTCPeerConnection()
     let localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: args.isVideoCall})
+    .catch(userMediaErrorHandler)
+    if(!localStream) return
+
     args.ref.current.srcObject = localStream
-    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream))
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream as MediaStream))
 }
