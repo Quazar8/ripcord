633bdea4076cc4fa241e1fa13bd3a29dc3df0f20
Quazar8 <barcode12d@gmail.com>
2021-07-11 18:03:13 +0300

    Markup for the receiving call component

---
+++ client/components/chat/RightWindow/call/AcceptCallButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReceiveCallButton = () => {
+    return (
+        <button className = "receive-call-button">
+            <span>&#128222;</span>
+        </button>
+    )
+}
+
+export default ReceiveCallButton
--- client/components/chat/RightWindow/call/ReceivingCallBlock.tsx
+++ client/components/chat/RightWindow/call/ReceivingCallBlock.tsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import ProfilePic from '../../../user/ProfilePic'
 import { ChatDisplayProps } from '../ChatDisplay'
+import ReceiveCallButton from './AcceptCallButton'
+import HangUpButton from './HangUpButton'
 
 type Props = {
     receivingCall: ChatDisplayProps['callState']['receivingCall']
 }
 
 const ReceivingCallBlock = (props: Props) => {
+    if (!props.receivingCall) return null
+
     return (
         <div className = "receiving-call-block">
-            Receiving call component
+            <ProfilePic picNameOrJson = { props.receivingCall.callerProfilePic }/>
+            <h3>{ props.receivingCall.callerName }</h3>
+            <div className = "buttons-container">
+                <ReceiveCallButton />
+                <HangUpButton />
+            </div>
         </div>
     )
 }
