9b9c97ffcf31312c60ce821c5e3e07e1ba9b5f7d
Quazar8 <barcode12d@gmail.com>
2021-07-13 19:12:31 +0300

    Hangup button only to receive a hangupcall function as a prop to make it reusable

--- client/components/chat/RightWindow/call/HangUpButton.tsx
+++ client/components/chat/RightWindow/call/HangUpButton.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
-import { DenyingCallPayload, WSDataType, WSMessage } from '../../../../../server/types/WebsocketTypes'
-import { sendSocketMessage } from '../../../../socket/socket'
 
 type Props = {
-    removeCallInfoStore: () => void
-    callerId: string
-    recipientId: string
+    hangUpCall: () => void
 }
 
 const HangUpButton = (props: Props) => {
-    const hangUpCall = () => {
-        props.removeCallInfoStore()
-        const msg: WSMessage<DenyingCallPayload> = {
-            type: WSDataType.RECEIVING_CALL_DENIED,
-            payload: {
-                callerId: props.callerId,
-                recipientId: props.recipientId
-            }
-        }
-
-        sendSocketMessage(msg)
-    }
-
     return (
-        <button onClick = { hangUpCall } className = "hang-up-button">
+        <button onClick = { props.hangUpCall } className = "hang-up-button">
             <span>&#128222;</span>
         </button>
     )
--- client/components/chat/RightWindow/call/ReceivingCallBlock.tsx
+++ client/components/chat/RightWindow/call/ReceivingCallBlock.tsx
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react'
+import { DenyingCallPayload, WSDataType, WSMessage } from '../../../../../server/types/WebsocketTypes'
+import { sendSocketMessage } from '../../../../socket/socket'
 import ProfilePic from '../../../user/ProfilePic'
 import { ChatDisplayProps } from '../ChatDisplay'
 import { RightWindowContext } from '../RightWindow'
@@ -14,17 +16,26 @@ const ReceivingCallBlock = (props: Props) => {
 
     const { callFns, userId } = useContext(RightWindowContext)
 
+    const hangUpCall = () => {
+        callFns.removeCallInfoStore()
+        const msg: WSMessage<DenyingCallPayload> = {
+            type: WSDataType.RECEIVING_CALL_DENIED,
+            payload: {
+                callerId: props.receivingCall.callerId,
+                recipientId: userId
+            }
+        }
+
+        sendSocketMessage(msg)
+    }
+
     return (
         <div className = "receiving-call-block">
             <ProfilePic picNameOrJson = { props.receivingCall.callerProfilePic }/>
             <h3>{ props.receivingCall.callerName }</h3>
             <div className = "buttons-container">
                 <AnswerCallButton />
-                <HangUpButton 
-                    removeCallInfoStore = { callFns.removeCallInfoStore }
-                    recipientId = { userId }
-                    callerId = { props.receivingCall.callerId }
-                />
+                <HangUpButton hangUpCall = { hangUpCall }/>
             </div>
         </div>
     )
