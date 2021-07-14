9566357925fb105f88513dec967ee5d33a99d04a
Quazar8 <barcode12d@gmail.com>
2021-07-14 21:32:32 +0300

    Changing logosvg attributes to comply with react

--- client/components/svgs/LogoSvg.tsx
+++ client/components/svgs/LogoSvg.tsx
@@ -7,8 +7,8 @@ const LogoSvg = () => {
             <rect x="96.7731" y="5" width="70.48" height="158.697" transform="rotate(28 96.7731 5)" />
             <rect x="159" y="38.097" width="70.48" height="158.697" transform="rotate(-28 159 38.097)" />
             <ellipse cx="158.5" cy="93.5" rx="89.5" ry="61.5"/>
-            <path d="M97.3187 104L81 75L119.44 79.9457L147 104H97.3187Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M221.743 103.892L238.507 73.2109L199.229 78.3294L171 103.733L221.743 103.892Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M97.3187 104L81 75L119.44 79.9457L147 104H97.3187Z" stroke="black" strokeLinecap="round"/>
+            <path d="M221.743 103.892L238.507 73.2109L199.229 78.3294L171 103.733L221.743 103.892Z" stroke="black" strokeLinejoin="round"/>
         </svg>
     )
 }
